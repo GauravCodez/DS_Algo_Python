f79145a8c6558c556e02d63edc0c2360c85b3bc9
GauravChamp <gauravmehta1202@gmail.com>
2022-09-29 13:55:28 +0530

    Linked List Append

---
+++ Practice/LinkedList/Linked_List_Append.py
@@ -0,0 +1,40 @@
+# Program to append the linked list
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def printList(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+
+my_linked_list = LinkedList(4)
+my_linked_list.append(5)
+my_linked_list.append(6)
+my_linked_list.append(7)
+my_linked_list.append(8)
+
+my_linked_list.printList()
